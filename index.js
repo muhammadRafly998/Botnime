47e482a3457e6e92fdd29d4fde563cf461cf119b
muhammadRafly998 <75070747+muhammadRafly998@users.noreply.github.com>
2020-12-13 21:27:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const { color, options } = require('./tools')
+const { ind, eng } = require('./message/text/lang/')
+const figlet = require('figlet')
+const msgHandler = require('./message')
+const config = require('./config.json')
+const ownerNumber = config.ownerBot
+
+const start = async (bocchi = new Client()) => {
+    console.log(color(figlet.textSync('BocchiBot', 'Larry 3D'), 'cyan'))
+    console.log('[BOCCHI]', color('BocchiBot is now online!'))
+
+    // Force it to keep the current session
+    bocchi.onStateChanged((state) => {
+        console.log('[BOCCHI STATE]', state)
+        if (state === 'UNPAIRED') bocchi.forceRefocus()
+        if (state === 'CONFLICT') bocchi.forceRefocus()
+        if (state === 'UNLAUNCHED') bocchi.forceRefocus()
+    })
+
+    // Set all received message to seen
+    bocchi.onAck((x) => {
+        const { to } = x
+        if (x !== 3) bocchi.sendSeen(to)
+    })
+
+    // Listening added to group
+    bocchi.onAddedToGroup(async (chat) => await bocchi.sendText(chat.groupMetadata.id, ind.addedGroup(chat)))
+
+    // Listening on message
+    bocchi.onMessage((message) => {
+        bocchi.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 100) {
+                    console.log('[BOCCHI]', color(`Loaded message reach ${msg}, cuting message cache...`, 'yellow'))
+                    bocchi.cutMsgCache()
+                        .then(() => console.log('[BOCCHI]', color('Cache deleted!', 'yellow')))
+                }
+            })
+        msgHandler(bocchi, message) // Message handler
+    })
+
+    // Block person who called bot
+    bocchi.onIncomingCall(async (callData) => {
+        await bocchi.sendText(callData.peerJid, ind.blocked(ownerNumber))
+        await bocchi.contactBlock(callData.peerJid)
+            .then(() => console.log(color('[BLOCK]', 'red'), color(`${callData.peerJid} has been blocked. Reason:`, 'yellow'), color('CALLING THE BOT', 'cyan')))
+    })
+}
+
+// Creating session
+create(options(start))
+    .then((bocchi) => start(bocchi))
+    .catch((err) => new Error(err))
